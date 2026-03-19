5816b4f33868d13f5c78b689ea7b057955926386
root <soumiyajit@gmail.com>
2026-03-19 10:55:12 +0000

    custom action test

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         // Use the hardcoded fallback here too
-        const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/academia_synapse';
+        // const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/academia_synapse';
+        const uri = "mongodb+srv://admin:Password123@cluster0.mongodb.net/synapse";
         await mongoose.connect(uri);
         console.log('MongoDB Connected via config/db.js');
     } catch (err) {
